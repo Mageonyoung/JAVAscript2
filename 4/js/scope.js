29a254ad84221f0aabb66b68b95c587db45fc1bc
Mageonyoung <144089134+Mageonyoung@users.noreply.github.com>
2023-10-17 21:13:00 +0900

    Update scope.js

--- 4/js/scope.js
+++ 4/js/scope.js
@@ -1,5 +1,7 @@
-function sum (num1, num2){
-    var result = num1 + num2;//지역변수
+function sum(num1, num2) {
+    var result = num1 + num2; // 지역 변수
+    return result; // 결과 반환
 }
-sum(10,20);
-console.log(result);
+
+var resultValue = sum(10, 20);
+console.log(resultValue);
